ec72d8595112e7dabd023f8b676c1f2d48377d09
saqlainhaider27 <sp24-bse-109@cuilahore.edu.pk>
2024-11-15 15:59:23 +0500

    Server

--- src/Server.java
+++ src/Server.java
@@ -1,23 +1,29 @@
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
-
 
 public class Server {
 
-    ServerSocket serverSocket = null;
+    private ServerSocket socket = null;
+    private DataInputStream in = null;
+
     public void CreateServer(int port){
         try {
-            serverSocket = new ServerSocket(port);
+            socket = new ServerSocket(port);
             System.out.println("Server is listening on port " + port);
-            Socket clientSocket = serverSocket.accept();
+            Socket clientSocket = socket.accept();
             System.out.println("Client connected: " + clientSocket.getInetAddress().getHostAddress());
             String line = "";
-            while (!line.equals("Close")) {
-                Scanner scanner = new Scanner(System.in);
-                line = scanner.nextLine();
-                System.out.println(line);
+            while (!line.equals("Over")) {
+                try {
+                    line = in.readUTF();
+                    System.out.println(line);
+                } catch(IOException i) {
+                    System.out.println(i);
+                }
             }
+            System.out.println("Closing connection");
+            socket.close();
+            in.close();
         } catch (IOException e) {
             System.err.println("Could not listen on port " + port);
 
--- src/User.java
+++ src/User.java
@@ -1,2 +1,39 @@
+import java.net.*;
+import java.io.*;
+
 public class User {
+    private Socket socket = null;
+    private DataInputStream input = null;
+    private DataOutputStream out = null;
+
+    public User(String address, int port) {
+        try {
+            socket = new Socket(address, port);
+            System.out.println("Connected");
+            input = new DataInputStream(System.in);
+            out = new DataOutputStream(socket.getOutputStream());
+        } catch(UnknownHostException u) {
+            System.out.println(u);
+        } catch(IOException i) {
+            System.out.println(i);
+        }
+
+        String line = "";
+        while (!line.equals("Over")) {
+            try {
+                line = input.readLine();
+                out.writeUTF(line);
+            } catch(IOException i) {
+                System.out.println(i);
+            }
+        }
+
+        try {
+            input.close();
+            out.close();
+            socket.close();
+        } catch(IOException i) {
+            System.out.println(i);
+        }
+    }
 }
