7859598bd5ad24cef08c02d1bd39a5f987b7e782
saqlainhaider27 <sp24-bse-109@cuilahore.edu.pk>
2024-11-15 16:06:14 +0500

    Client

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Server server = new Server();
         server.CreateServer(1111);
+        User sender = new User("192.168.100.1", 1111);
     }
 }
